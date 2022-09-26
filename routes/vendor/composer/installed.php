d2ef5718c0723296faeae9ee1f28eb00fc007006
martinalmeida <martinalmeida56@gmail.com>
2022-09-25 22:18:27 -0500

    sistemas de rutas con libreria

---
+++ routes/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'easy-projects/simple-router' => array(
+            'pretty_version' => 'v7.2.1',
+            'version' => '7.2.1.0',
+            'reference' => '0cb33a23767385f0e655cd69376f5547710a4324',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../easy-projects/simple-router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
