ad401e5d9cb82e0a3ee803666490fe6b38c88919
EddierGM <89800783+EddierGM@users.noreply.github.com>
2022-09-28 13:46:37 -0500

    Add files via upload
    edit

---
+++ inc/vlappConfig.php
@@ -0,0 +1,39 @@
+<?php
+// Definir el uso horario o timezone del sistema
+date_default_timezone_set('America/Bogota');
+
+// Lenguaje
+define('LANG', 'es');
+
+// Rutas principales
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/vlapp/');
+define('APP', ROOT_PATH . 'app/');
+define('RESOURCES', ROOT_PATH . 'resources/');
+define('ASEETS', ROOT_PATH . 'aseets/');
+define('INC', ROOT_PATH . 'inc/');
+define('PUBLIC_PATH', ROOT_PATH . 'public/');
+define('JS', PUBLIC_PATH . 'js/');
+define('VIEW', PUBLIC_PATH . 'views/');
+
+// Rutas de la app
+define('CONTROLLERS', APP . 'controllers/');
+define('INCLUDES', APP . 'includes/');
+define('LIBRARIES', APP . 'librarias/');
+define('MODELS', APP . 'models/');
+
+// // Rutas de archivos o assets con base URL
+define('ASSETS', RESOURCES . 'assets/');
+define('CSS', ASSETS . 'css/');
+define('IMG', ASSETS . 'imag/');
+define('FAVICON', ASSETS . 'favicon/');
+define('FONTS', ASSETS . 'fonts/');
+// define('JS', ASSETS . 'js/'); muestra error REVISAR
+define('PLUGINS', ASSETS . 'plugins/');
+define('UPLOADS', ASSETS . 'uploads/');
+
+// Credenciales de la base de datos
+define('HOST_DB', 'localhost');
+define('USER_DB', 'root');
+define('PASSWORD_DB', '');
+define('NAME_DB', 'irma');
+define('LDB_CHARSET', 'utf8');
