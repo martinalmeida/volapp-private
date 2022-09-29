852ee1b428687db94df362238b81c7ae41671ad8
martinalmeida <64678166+martinalmeida@users.noreply.github.com>
2022-09-29 11:03:42 -0500

    Template funciones para maquetar

---
+++ resources/components/cradBody.php
@@ -0,0 +1,19 @@
+<div class="subheader">
+    <h1 class="subheader-title">
+        <i class='fal fa-info-circle'></i> INICIO
+        <small>
+            SUB TITULO
+        </small>
+    </h1>
+</div>
+<div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
+    <h3 class="mb-g">
+        DIV VACIO
+    </h3>
+    <p>
+        DIV VACIO.
+    </p>
+</div>
+<h3>
+    Titulo abajo
+</h3>
