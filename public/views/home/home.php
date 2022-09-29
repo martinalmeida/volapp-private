9cc6279740ce52594f8311bbda45a0672ceb773c
EddierGM <eddiergonzalez10@gmail.com>
2022-09-29 11:54:29 -0500

    vista home

---
+++ public/views/home/home.php
@@ -0,0 +1,30 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/vlapp/inc/vlappConfig.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . INCLUDES . 'template.php');
+Template::Head('404');
+Template::startBody();
+?>
+
+<!-- ========== Inicio Componente de Vista ========== -->
+<div class="subheader">
+    <h1 class="subheader-title">
+        <i class='fal fa-info-circle'></i> INICIO
+        <small>
+            SUB TITULO
+        </small>
+    </h1>
+</div>
+<div class="fs-lg fw-300 p-5 bg-white border-faded rounded mb-g">
+    <h3 class="mb-g">
+        DIV VACIO
+    </h3>
+    <p>
+        DIV VACIO.
+    </p>
+</div>
+<h3>
+    Titulo abajo
+</h3>
+<!-- ========== Fin Componente de Vista ========== -->
+
+<?= Template::endBody(); ?>
