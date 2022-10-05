580c4f7fce021f5ba1ea95d9c7de8a87b6b642b8
martinalmeida <64678166+martinalmeida@users.noreply.github.com>
2022-10-05 09:00:16 -0500

    Se inicia el desarrollo por completo con la estructura lista

--- src/routing/config.php
+++ src/routing/config.php
@@ -24,19 +24,30 @@ class InitRouting
 
     public function startRouting()
     {
+        // --Validamos si es una peticion HTTPS--
         if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-            $this->tokenUser = $_POST["token"];
-            if (self::TOKEN === $this->tokenUser) {
-                if (URL !== '/') {
-                    require_once(__DIR__ . '/routing.php');
-                    $router = new Routing;
-                    $router->run();
+            // --Comprobar si hay un metodo autorizado GET && POST
+            if ($_SERVER['REQUEST_METHOD'] == 'GET' or $_SERVER['REQUEST_METHOD'] == 'POST') {
+                // --Comprobar el token de la plataforma
+                $this->tokenUser = $_POST["token"];
+                if (self::TOKEN === $this->tokenUser) {
+                    // --Validacion de ruta
+                    if (URL !== '/') {
+                        // --Ejecucion del router
+                        require_once(__DIR__ . '/routing.php');
+                        $router = new Routing;
+                        $router->run();
+                    } else {
+                        $request['status'] = '0';
+                        echo json_encode($request);
+                    }
                 } else {
-                    $request['status'] = '0';
-                    echo json_encode($request);
+                    echo json_encode('NO HAY TOKEN QUE COINCIDA');
+                    $this->tokenUser = NULL;
+                    exit;
                 }
             } else {
-                echo json_encode('NO HAY TOKEN QUE COINCIDA');
+                echo json_encode('SERVICIOS DENEGADOS POR NO CONTENER UN METODO VALIDO');
                 $this->tokenUser = NULL;
                 exit;
             }
