c05ba56a786df38f1848ee7b7928e25646f380ea
martinalmeida <64678166+martinalmeida@users.noreply.github.com>
2022-12-11 21:10:26 -0500

    reportes 1%

--- public/views/informes/relacionAlquiler.php
+++ public/views/informes/relacionAlquiler.php
@@ -12,7 +12,7 @@ $view->initializationView();
 <!-- ========== Inicio Componente de Vista ========== -->
 <div class="subheader">
     <h1 class="subheader-title">
-        <i class="fal fa-info-circle"></i> Informe de Relación
+        <i class="fal fa-info-circle"></i> Informe de Relación por Alquiler
     </h1>
 </div>
 
@@ -29,34 +29,20 @@ $view->initializationView();
             </div>
             <div class="panel-container show">
                 <div class="panel-content">
-                    <div class="panel-content">
-                        <div class="card-group">
-                            <div class="card bg-success">
-                                <div class="card-body text-center">
-                                    <h3 class="card-title text-white">Relación por Alquiler</h3>
-                                    <h1 class="text-white"><i class="fal fa-hands-usd fa-4x"></i></i></h1>
-                                    <p class="card-text text-white">En el modo alquiler se calcula el Stand By de horas, u horometro final menos horometro inicial.</p>
-                                    <button type="button" class="btn btn-outline-light btn-pills waves-effect waves-themed" onclick="getRuta(1);">Ingresar al Informe de Relación por Alquiler</button>
-                                </div>
+                    <form id="frmRegistro">
+                        <div class="form-row">
+                            <div class="col-md-6 mb-3">
+                                <label class="form-label">Placa o # de Registro:</label>
+                                <select class="custom-select form-control" id="placaInsert" name="placaInsert">
+                                </select>
                             </div>
-                            <div class="card bg-info">
-                                <div class="card-body text-center">
-                                    <h3 class="card-title text-white">Relación por Flete</h3>
-                                    <h1 class="text-white"><i class="fal fa-route fa-4x"></i></h1>
-                                    <p class="card-text text-white">En el modo flete se calcula solo un valor pactado por viaje con el socio o dueño del vehículo.</p>
-                                    <button type="button" class="btn btn-outline-light btn-pills waves-effect waves-themed" onclick="getRuta(2);">Ingresar al Informe de Relación por Fletes</button>
-                                </div>
-                            </div>
-                            <div class="card bg-primary">
-                                <div class="card-body text-center">
-                                    <h3 class="card-title text-white">Relación por Movimiento</h3>
-                                    <h1 class="text-white"><i class="fal fa-truck-container fa-4x"></i></h1>
-                                    <p class="card-text text-white">Calculo de nro de viajes, tarifa de ruta pactada, metraje cubico y kilometros recorridos.</p>
-                                    <button type="button" class="btn btn-outline-light btn-pills waves-effect waves-themed" onclick="getRuta(3);">Ingresar al Informe de Relación por Movimientos</button>
-                                </div>
+                            <div class="col-md-6 mb-3">
+                                <label class="form-label">Asignar Contrato:</label>
+                                <select class="custom-select form-control" id="contratoInsert" name="contratoInsert">
+                                </select>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
