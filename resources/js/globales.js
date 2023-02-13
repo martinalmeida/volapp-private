c21d5bf2ea933f2fab17c36d2623d75e6fdd51fa
martinalmeida <martinalmeida56@gmail.com>
2023-02-12 22:59:09 -0500

    local and update

--- resources/js/globales.js
+++ resources/js/globales.js
@@ -1,5 +1,5 @@
 // --URL principal del servidor para peticiones--
 // Desarrollo:
-//var urlBase = "http://localhost/volapp/";
+var urlBase = "http://localhost/volapp/";
 // Produccion:
-var urlBase = "http://192.168.1.249/volapp/";
+// var urlBase = "http://192.168.1.249/volapp/";
